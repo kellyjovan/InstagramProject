51def042717bccc11747553d6db86d0aaf9bbe56
shirokane <jcanete@aoiths.org>
2015-06-29 16:43:12 -0400

    rrhjbtwe

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+$('#wrap').tubular({videoId: 'ijOoE9zH9W4',ratio: 16/9});
+// $('#wrap').tubular({videoId: 'TpGbx4fxogE',ratio: 4/3});
+//ijOoE9zH9W4
+
+$('.btn').click(function(){
+  $('.container').toggle("slide");
+});
