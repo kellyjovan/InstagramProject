8182ec5df21282e8393ad48f946195d223fc8394
github username <github email address>
2015-06-30 19:22:14 -0400

    added autofocus

--- public/js/script.js
+++ public/js/script.js
@@ -11,20 +11,32 @@ $(document).ready(function(){
     $('#con-wrap').show('slow');
   });
 
-  $.ajax({
-    url: 'http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC&limit=21',
-    method: 'GET',
-    dataType: 'JSON',
-    success: function(data){
-      $.each(data.data, function(i,v){
-        addImages(data.data[i].images.original_still.url, data.data[i].images.original.url);
-      });
-    },
-    error: function(xhr, ajaxOptions, thrownError){
-      console.log(xhr.status);
-      console.log(thrownError);
-    }
+  $('#searchBtn').click(function(){
+    var input = $('#search').val();
+    $('#search').val('');
+    $('#result').text(input);
+    searchGiphy(input);
+    $('.content').empty();
   });
+
+  function searchGiphy(search){
+    var api = "http://api.giphy.com/v1/gifs/search?q=_SEARCH_&api_key=dc6zaTOxFJmzC&limit=36";
+    api = api.replace('_SEARCH_', search);
+    $.ajax({
+      url: api,
+      method: 'GET',
+      dataType: 'JSON',
+      success: function(data){
+        $.each(data.data, function(i,v){
+          addImages(data.data[i].images.original_still.url, data.data[i].images.original.url);
+        });
+      },
+      error: function(xhr, ajaxOptions, thrownError){
+        console.log(xhr.status);
+        console.log(thrownError);
+      }
+    });
+  }
 
   function addImages(url, url2){
     var imgBox = '<div class="col-sm-4 item"><a class="fancybox" rel="group" href="_URL2_"><img src="_URL_" alt="" /></a></div>';
